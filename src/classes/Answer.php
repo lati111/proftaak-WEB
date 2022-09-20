b1cc8a5223aca7ccbd7ce1355cdf934922a63c60
lati111 <jvzelst111@gmail.com>
2022-09-20 11:34:20 +0200

    Added Answer class foundations

---
+++ src/classes/Answer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Answer;
+
+use Database\Database;
+use Question\Question;
+use PDO;
+
+class Answer
+{
+    private int $id;
+    private Question $question;
+    private string $antwoord;
+    private int $votes;
+    private $error;
+
+    function __construct(int $id, Question $question)
+    {
+        $this->id = $id;
+        $this->question = $question;
+        $q_a = new Database("q&a");
+        $db = $q_a->getConn();
+
+        $sql = "SELECT antwoord, votes FROM answer WHERE idAnswer = :id";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            $developerData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $this->vraag = $developerData["antwoord"];
+            $this->vraag = $developerData["votes"];
+        } else {
+            $this->error = "No answer under that id found.";
+        }
+    }
+
+    public function getVraag()
+    {
+        return $this->vraag;
+    }
+
+    public function getDeveloper()
+    {
+        return $this->developer;
+    }
+
+    public function getError()
+    {
+        $error = $this->error;
+        $this->error = null;
+        return $error;
+    }
+}
