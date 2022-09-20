874f64b15fa0c2dbfe10dae386c596d5fc88ee9b
lati111 <jvzelst111@gmail.com>
2022-09-20 11:50:42 +0200

    changed file structure

---
+++ src/pages/register/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Q&A - register</title>
+</head>
+
+<body>
+    <h1>Registreer een account</h1>
+    <table>
+        <tbody>
+            <tr>
+                <td>Gebruikersnaam:</td>
+                <td><input type="text" name="nickname"></td>
+            </tr>
+            <tr>
+                <td>Volledige naam:</td>
+                <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td>Email adres:</td>
+                <td><input type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td>Wachtwoord:</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><button>registreer</button></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
---
+++ src/scripts/ajax.js
@@ -0,0 +1,14 @@
+async function ajax(Function, Parameters = []) {
+    let formData = new FormData();
+    formData.append('function', Function);
+    formData.append('parameters', JSON.stringify(Parameters));
+
+    const ajaxPath = "/src/backend/ajax.php"
+    let promise = await fetch(ajaxPath, {
+        method: 'post',
+        body: formData,
+        headers: {'Accept': 'application/json'}})
+        .then((response)=>response.json())
+        .then((responseJson)=>{return responseJson});
+    return promise;
+}
