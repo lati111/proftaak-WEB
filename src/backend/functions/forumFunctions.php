ed6bc7817b0feaa1d1cacfafd8eb664b29a372ea
lati111 <jvzelst111@gmail.com>
2022-09-27 13:55:15 +0200

    added forumFunctions.php

---
+++ src/backend/functions/forumFunctions.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+require "../../vendor/autoload.php";
+
+use Modules\Database\Database;
+use Modules\Forum\Answer\Answer;
+use Modules\Forum\Question\Question;
+
+function getQuestionCount() {
+
+}
---
+++ src/pages/forum/index.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+session_start();
+?>
+<!doctype html>
+
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proftaak forum</title>
+
+</head>
+
+<body>
+    <section id="header">
+        <div id="bannerSection">
+            <h1>Proftaak forums</h1>
+        </div>
+        <div id="accountSection">
+
+        </div>
+    </section>
+
+    <section id="mainBody">
+        <div id="forumTableContainer"></div>
+    </section>
+
+</body>
+
+</html>
