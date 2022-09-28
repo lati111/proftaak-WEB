e6b0198574a5b43169cfa581bd629460363123a1
lati111 <jvzelst111@gmail.com>
2022-09-28 12:06:58 +0200

    added ajax for login

--- src/backend/ajax.php
+++ src/backend/ajax.php
@@ -19,7 +19,6 @@ switch ($_POST["function"]) {
     case "registerDeveloper":
         $valid = true;
         if (!is_null($parameters["name"])) {
-
             if (!preg_match('/^[a-zA-Z ]+$/', $parameters["name"])) {
                 $response = "Parameter 'name' may not contain numbers or special characters";
                 $valid = false;
@@ -43,12 +42,19 @@ switch ($_POST["function"]) {
 
         if ($valid) {
             $result = registerDeveloper($parameters["name"], $parameters["email"], $parameters["password"], $parameters["nickname"]);
-            
+
             if ($result === false) {
                 $response = $_SESSION["error"];
             }
         }
-
+        break;
+    case "developerLogin":
+        if (is_null($parameters["email"])) {
+            $response = "Parameter 'email' cannot be empty";
+        } 
+        else if (is_null($parameters["password"])) {
+            $response = "Parameter 'password' cannot be empty";
+        }
         break;
     default:
         $response = "No such function available";
