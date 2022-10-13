a3d4d8cfd9a505f67be24cba4d8cbf43cde59d4c
lati111 <jvzelst111@gmail.com>
2022-10-13 13:59:41 +0200

    added vote functions to developer

--- src/backend/modules/Users/Developer.php
+++ src/backend/modules/Users/Developer.php
@@ -1,11 +1,14 @@
 <?php
-declare (strict_types = 1);
+
+declare(strict_types=1);
 
 namespace Modules\Developer;
 
 require "../../vendor/autoload.php";
 
+use Exception;
 use Modules\Database\Database;
+use Modules\Forum\Answer\Answer;
 use PDO;
 
 class Developer
@@ -14,7 +17,6 @@ class Developer
     private string $nickname;
     private string $name;
     private string $email;
-    private $error;
 
     function __construct(int $id)
     {
@@ -32,8 +34,56 @@ class Developer
             $this->nickname = $developerData["nickname"];
             $this->email = $developerData["email"];
         } else {
-            $this->error = "No developer under that id found.";
+            throw new Exception("No developer under that id found");
         }
+    }
+
+    public function hasVoted(Answer $answer): bool
+    {
+        $q_a = new Database("q&a");
+        $db = $q_a->getConn();
+
+        $answerID = $answer->getID();
+        $sql = "SELECT * FROM voteLog WHERE idDeveloper = :idDeveloper AND idAnswer = :idAnswer";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":idDeveloper", $this->id);
+        $stmt->bindParam(":idAnswer", $answerID);
+        $stmt->execute();
+        return ($stmt->rowCount() > 0);
+    }
+
+    public function vote(Answer $answer): bool
+    {
+        if ($this->hasVoted($answer)) {
+            throw new Exception("You already voted on this!", 2);
+        }
+
+        $q_a = new Database("q&a");
+        $db = $q_a->getConn();
+
+        $answerID = $answer->getID();
+        $sql = "INSERT INTO voteLog VALUES (:idAnswer, :idDeveloper)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":idDeveloper", $this->id);
+        $stmt->bindParam(":idAnswer", $answerID);
+        return $stmt->execute();
+    }
+
+    public function unvote(Answer $answer): bool
+    {
+        if (!$this->hasVoted($answer)) {
+            throw new Exception("You cannot delete something you haven't voted on!", 3);
+        }
+
+        $q_a = new Database("q&a");
+        $db = $q_a->getConn();
+
+        $answerID = $answer->getID();
+        $sql = "DELETE FROM voteLog WHERE idDeveloper = :idDeveloper AND idAnswer = :idAnswer";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":idDeveloper", $this->id);
+        $stmt->bindParam(":idAnswer", $answerID);
+        return $stmt->execute();
     }
 
     public function getName(): string
@@ -54,12 +104,5 @@ class Developer
     public function getEmail()
     {
         return $this->email;
-    }
-
-    public function getError()
-    {
-        $error = $this->error;
-        $this->error = null;
-        return $error;
     }
 }
